5511857fd3de5ad5ec0cd6738f791609d41990a3
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-12 17:28:27 -0500

    fixed backend and front end routes for single project view, created basic single-product react component

--- server/api/products.js
+++ server/api/products.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
-const {Product} = require('../db/models/product')
+const Product = require('../db/models/product')
+const Review = require('../db/models/review')
+const Category = require('../db/models/category')
+const Order = require('../db/models/order')
+
 module.exports = router
 
 router.get('/:productId', async (req, res, next) => {
   try {
-    const product = await Product.findByPk(req.params.productId)
+    const product = await Product.findByPk(req.params.productId, {
+      include: [Review, Order, Category]
+    })
+
     res.json(product.dataValues).end()
   } catch (err) {
     next(err)
