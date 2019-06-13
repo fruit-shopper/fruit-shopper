c63e2d93306ac362ad488935f442520abdf2f3cf
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-12 20:09:51 -0500

    single product component dispays picture and basic information about the product

--- client/components/Single-Product.js
+++ client/components/Single-Product.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchProduct} from '../store/product'
+import {Grid, Image} from 'semantic-ui-react'
 
 class SingleProduct extends Component {
   constructor() {
@@ -9,19 +10,29 @@ class SingleProduct extends Component {
       name: ''
     }
   }
-  render() {
-    console.log('---> ', this.props.product)
-    return <div />
-  }
 
   componentDidMount() {
-    console.log('this.props', this.props)
     const productId = this.props.match.params.productId
     this.props.loadProduct(productId)
   }
+
+  render() {
+    console.log('this.props.image:', this.props.product)
+    return (
+      <Grid>
+        <Grid.Column floated="left" width={5}>
+          <Image src={this.props.product.image} />
+        </Grid.Column>
+        <Grid.Column floated="right" width={5}>
+          <h4>{this.props.product.name}</h4>
+          <p>Description: {this.props.product.description}</p>
+          <p>Price: ${this.props.product.price}</p>
+        </Grid.Column>
+      </Grid>
+    )
+  }
 }
 
-//doublecheck reducer names/structure
 const mapStateToProps = state => ({
   product: state.product
 })
