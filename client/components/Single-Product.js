f8ce904d030cc18c86efdda210430933382f0e02
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-12 22:26:38 -0500

    improved single product page, added style to login input fields and button, changed navbar header

--- client/components/Single-Product.js
+++ client/components/Single-Product.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchProduct} from '../store/product'
-import {Grid, Image} from 'semantic-ui-react'
+import {Grid, Image, Button} from 'semantic-ui-react'
+import Review from './Review'
 
 class SingleProduct extends Component {
   constructor() {
@@ -17,19 +18,34 @@ class SingleProduct extends Component {
   }
 
   render() {
-    console.log('this.props.image:', this.props.product)
-    return (
-      <Grid>
-        <Grid.Column floated="left" width={5}>
-          <Image src={this.props.product.image} />
-        </Grid.Column>
-        <Grid.Column floated="right" width={5}>
-          <h4>{this.props.product.name}</h4>
-          <p>Description: {this.props.product.description}</p>
-          <p>Price: ${this.props.product.price}</p>
-        </Grid.Column>
-      </Grid>
-    )
+    const reviews = this.props.product.reviews
+    if (reviews === undefined) {
+      return <p>This product has no reviews</p>
+    } else {
+      // if (reviews.length === 0) {
+      //   return <p>This product has no reviws.</p>;
+      // } else {
+      return (
+        <Grid>
+          <Grid.Column width={5} />
+          <Grid.Column className="ui centered row" width={3}>
+            <Image src={this.props.product.image} />
+          </Grid.Column>
+          <Grid.Column className="ui centered row" width={3}>
+            <h4>{this.props.product.name}</h4>
+            <p>Description: {this.props.product.description}</p>
+            <p>Price: ${this.props.product.price}</p>
+            <Button color="green">Add To Cart</Button>
+          </Grid.Column>
+          <Grid.Column width={5} />
+          <Grid.Row className="ui centered column" width={10}>
+            <h3>Product Reviews: </h3>
+
+            {reviews.map(review => <Review review={review} key={review.id} />)}
+          </Grid.Row>
+        </Grid>
+      )
+    }
   }
 }
 
@@ -38,7 +54,6 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => {
-  console.log('MapDispatch')
   return {
     loadProduct: productId => dispatch(fetchProduct(productId))
   }
