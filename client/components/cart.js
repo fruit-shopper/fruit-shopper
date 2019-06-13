ca2e6f852a5acf1f8b9efb3cf919ada51ee13f78
Monica Barboza <barboza.monica2@gmail.com>
2019-06-13 16:56:58 -0500

    testing seed travis

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Button, Header} from 'semantic-ui-react'
 
 export class Cart extends Component {
   render() {
-    return <div />
+    return (
+      <div id="allProductsPage">
+        <div id="header">
+          <Header as="h1">Your Cart</Header>
+        </div>
+      </div>
+    )
   }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Cart)
+export default Cart
+//connect(mapStateToProps, mapDispatchToProps)(Cart)
