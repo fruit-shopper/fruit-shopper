d85a76d972ba72751f64c3a6dd928279212bc677
Tao Wang <tao.wang6000@gmail.com>
2019-06-14 15:49:35 -0500

    differenciate users as visiter, user and admin, rendering different navbar

--- client/routes.js
+++ client/routes.js
@@ -10,9 +10,10 @@ import {
   OpenOrders,
   PastOrders,
   SingleProduct,
-  Cart
+  Cart,
+  AdminHome
 } from './components'
-import {me, fetchProducts} from './store'
+import {me} from './store'
 
 /**
  * COMPONENT
@@ -24,26 +25,47 @@ class Routes extends Component {
 
   render() {
     // console.log('props in routes', this.props)
-    const {isLoggedIn} = this.props
+    const {isLoggedIn, isAdmin} = this.props
 
     return (
       <Switch>
-        {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-
-        {isLoggedIn && (
+        {!isLoggedIn &&
+          !isAdmin && (
+            <Switch>
+              {/* Routes placed here are available to all visitors */}
+              <Route path="/products/:productId" component={SingleProduct} />
+              <Route path="/products" component={AllProducts} />
+              <Route path="/home" component={UserHome} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/login" component={Login} />
+              <Route path="/signup" component={Signup} />
+            </Switch>
+          )}
+        {isLoggedIn &&
+          !isAdmin && (
+            <Switch>
+              {/* Routes placed here are only available after logging in */}
+              <Route path="/products/:productId" component={SingleProduct} />
+              <Route path="/products" component={AllProducts} />
+              <Route path="/home" component={UserHome} />
+              <Route path="/open_orders_user" component={OpenOrders} />
+              <Route path="/past_orders_user" component={PastOrders} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/login" component={Login} />
+            </Switch>
+          )}
+        {isAdmin && (
           <Switch>
-            {/* Routes placed here are only available after logging in */}
+            {/* Routes placed here are only available for admin */}
             <Route path="/products/:productId" component={SingleProduct} />
             <Route path="/products" component={AllProducts} />
             <Route path="/home" component={UserHome} />
             <Route path="/open_orders_user" component={OpenOrders} />
             <Route path="/past_orders_user" component={PastOrders} />
             <Route path="/cart" component={Cart} />
+            <Route path="/admin" component={AdminHome} />
           </Switch>
         )}
-
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
       </Switch>
@@ -59,7 +81,8 @@ const mapState = state => {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
     // add "isAdmin" for admin
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    isAdmin: !!state.user.id && state.user.admin === true
   }
 }
 
