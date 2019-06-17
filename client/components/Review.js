066c43b3fafb02f1f6c646c6f1260e8ec0ebad78
Olga Shiryaeva <oshiryaeva@protonmail.com>
2019-06-17 12:10:37 -0500

    deleted extra rating line in reviews and updated product status

--- client/components/Review.js
+++ client/components/Review.js
@@ -4,7 +4,6 @@ import {Container, Rating, Divider} from 'semantic-ui-react'
 export const Review = props => {
   return (
     <Container className="centered" text>
-      <Divider />
       <h4 align="left">
         review by <i>{props.review.user.name}</i>
       </h4>
@@ -15,10 +14,6 @@ export const Review = props => {
       {/* <div className="ui star rating" data-rating="3"></div> */}
       <div className="justify-text">{props.review.text}</div>
       <Divider />
-      <h3 align="right">
-        Rating:{' '}
-        <Rating icon="star" rating={props.review.rating} maxRating={5} />
-      </h3>
     </Container>
   )
 }
