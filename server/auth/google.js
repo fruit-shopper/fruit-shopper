a00647f1514be67567073534b9fc6283a0fcdde0
Olga <41551585+olgashi@users.noreply.github.com>
2019-07-20 17:13:41 -0500

    Update google.js

--- server/auth/google.js
+++ server/auth/google.js
@@ -24,7 +24,8 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK
+//     callbackURL: process.env.GOOGLE_CALLBACK
+    callbackURL:  'https://fruit-shopper.herokuapp.com/auth/google/callback'
   }
 
   const strategy = new GoogleStrategy(
